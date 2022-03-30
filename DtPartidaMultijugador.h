bdc2d6ef882e160bf08aa38482b7d1a1fe22cbf9
ShodytheWolf <49129728+ShodytheWolf@users.noreply.github.com>
2022-03-30 16:32:45 -0300

    Update DtPartidaMultijugador.h

--- DtPartidaMultijugador.h
+++ DtPartidaMultijugador.h
@@ -1,12 +1,12 @@
 #ifndef DTPARTIDAMULTIJUGADOR_H
-#define DTPARTIDAMULTIJUGDARO_H
+#define DTPARTIDAMULTIJUGADOR_H
 #include "Header.h"
 
 class DtPartidaMultijugador : public Partida{
 
 private:
 bool transmitidaEnVivo;
-std::string nicknameJugadoresUnidos[0]; //en lugar del 0 hay un *
+std::string nicknameJugadoresUnidos[3]; //en lugar del 0 hay un *
 int cantidadJugadoresUnidos;
 
 };
