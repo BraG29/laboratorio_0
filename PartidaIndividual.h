77e6366fba2218ce7c05d4a40ba139b2be0dd1bf
Braian <braianbarrios_29@hotmail.com>
2022-04-01 17:27:53 -0300

    update PartidaIndividual.h

--- PartidaIndividual.h
+++ PartidaIndividual.h
@@ -4,10 +4,27 @@
 
 class PartidaIndividual: public Partida{
 
-private:
-bool continuaPartidaAnterior;
+    private:
 
-float darTotalHorasParticipantes();
+        bool continuaPartidaAnterior;
+
+    public:
+
+        //constructores:
+        PartidaIndividual();
+        PartidaIndividual(bool);
+
+        //destructor:
+        ~PartidaIndividual();
+
+        //getter:
+        bool getContinuarParitda();
+
+        //setter:
+        void setContinuarPartida(bool);
+
+        
+        virtual float darTotalHorasParticipantes();
 
 };
 
