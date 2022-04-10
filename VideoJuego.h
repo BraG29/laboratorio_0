742a02cd1f6053e00bd6d00aa915c8dfb0d5985a
jostelk <jostelk@users.noreply.github.com>
2022-04-10 18:14:11 -0300

    Update VideoJuego.h

--- VideoJuego.h
+++ VideoJuego.h
@@ -1,6 +1,6 @@
 #ifndef VIDEOJUEGO_H
 #define VIDEOJUEGO_H
-#include <list>
+
 #include "Header.h"
 #include "Enum.h"
 #include "DtVideoJuego.h"
@@ -30,15 +30,11 @@ public:
 	virtual DtVideojuego* getVideojuego();
 
 	void agregarPartida(Partida*);
-	bool existVideojuego(string nombre);
-
-	void SetJuegos(Partida *juego);
-    	list<Partida*> *GetJuegos() const;
 	
 private:
 	std::string nombre;
 	TipoJuego genero;
-	std::list<Partida*> *juegos;
+	
 	//relacion de videojuego con partidas:
 	Partida** partidas = new Partida*[MAX_PARTIDAS];
 };
